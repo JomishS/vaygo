3d3607ea3941636c55d19602388b2c667b2c34f8
JomishS <125350116+JomishS@users.noreply.github.com>
2023-05-22 14:01:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,22 +9,6 @@ var cors = require('cors');
 const bodyParser=require('body-parser')
 const {Configuration,OpenAIApi}=require('openai')
 
-
-// import express from 'express'
-// import cors from 'cors'
-// import bodyParser from 'body-parser'
-// import env from 'dotenv'
-// import {Configuration,OpenAIApi} from 'openai'
-// import mongoose from 'mongoose'
-// import session from 'express-session'
-// import MongoStore from 'connect-mongo'
-
-// const app=express() 
-// const mongoose=mongoose()
-// const session=session()
-// const MongoStore=MongoStore()
-
-
 const corsConfig = {
     origin: true,
     credentials: true,
@@ -53,7 +37,6 @@ const sessionStore=MongoStore.create({
 const sessionCollection = require('./sessionSchema');
  
 app.use(bodyParser.json())
-app.use(cors())
 // app.use(cors(corsConfig))
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())

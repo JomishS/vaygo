3ed3a85a92bbf818c970df51c86a16189803088b
JomishS <125350116+JomishS@users.noreply.github.com>
2023-05-23 11:17:51 +0530

    Update ViewPack.js

--- client/src/ViewPack.js
+++ client/src/ViewPack.js
@@ -12,7 +12,7 @@ export async function ViewPack()
 
     useEffect(()=>{
         
-        axios.get('https://vaygo.vercel.app/isEligibleWithSession',{withCredentials:true}).then((res)=>{
+        axios.get('/isEligibleWithSession',{withCredentials:true}).then((res)=>{
             console.log("User eligible for this page since session is present")
         },(err)=>{
             navigate('/login')

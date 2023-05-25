debe49ee45b5cc67e88fbcbdb880890e80ba021d
JomishS <125350116+JomishS@users.noreply.github.com>
2023-05-25 15:08:07 +0530

    Update loginRegister.js

--- client/src/loginRegister.js
+++ client/src/loginRegister.js
@@ -8,7 +8,7 @@ export function Login()
 {
     const navigate=useNavigate()
     useEffect(()=>{
-        axios.get('https://vaygo.vercel.app/isEligibleNoSession',{withCredentials:true}).then((res)=>{
+        axios.get('https://vaygo.online/isEligibleNoSession',{withCredentials:true}).then((res)=>{
             console.log(res.data)
         },(err)=>{
 //             console.log(err)
@@ -53,7 +53,7 @@ export function Login()
                     alert('Please complete the user details')
                 }
                 else{
-                    axios.post('https://vaygo.vercel.app/login',{email:email,password:pword}).then((res)=>{                
+                    axios.post('https://vaygo.online/login',{email:email,password:pword}).then((res)=>{                
                         if(res.data=='customer')
                         {
                         navigate('/CustMain')
